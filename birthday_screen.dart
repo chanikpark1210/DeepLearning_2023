c239455a6064171c2fe654f401703d027c12149c
CHANIK <127841500+chanikpark1210@users.noreply.github.com>
2023-06-22 13:55:12 +0900

    Add files via upload

---
+++ birthday_screen.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/email_screen.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+
+class BirthdayScreen extends StatefulWidget {
+  const BirthdayScreen({super.key});
+
+  @override
+  State<BirthdayScreen> createState() => _BirthdayScreenState();
+}
+
+class _BirthdayScreenState extends State<BirthdayScreen> {
+  final TextEditingController _birthdayController = TextEditingController();
+
+  DateTime initialDate =
+      DateTime.now().subtract(const Duration(days: 365 * 12));
+  @override
+  void initState() {
+    super.initState();
+    _setTextFieldDate(initialDate);
+  }
+
+  @override
+  void dispose() {
+    _birthdayController.dispose();
+    super.dispose();
+  }
+
+  void _onNextTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const EmailScreen(),
+      ),
+    );
+  }
+
+  void _onClearTap() {
+    _birthdayController.clear();
+  }
+
+  void _setTextFieldDate(DateTime date) {
+    final textDate = date.toString().split(" ").first;
+    _birthdayController.value = TextEditingValue(text: textDate);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          "Sign up",
+          style: TextStyle(),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            const Text(
+              "When's your birthday?",
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v8,
+            const Text(
+              "Your birthday won't be shown publicly.",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black54,
+              ),
+            ),
+            Gaps.v16,
+            TextField(
+              enabled: false,
+              controller: _birthdayController,
+              decoration: InputDecoration(
+                suffix: GestureDetector(
+                  onTap: _onClearTap,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.solidCircleXmark,
+                        color: Colors.grey.shade500,
+                        size: Sizes.size16 + Sizes.size2,
+                      ),
+                    ],
+                  ),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v16,
+            GestureDetector(
+                onTap: () => _onNextTap(context),
+                child: const FormButton(disabled: false))
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: SizedBox(
+          height: 270,
+          child: CupertinoDatePicker(
+            maximumDate: initialDate,
+            initialDateTime: initialDate,
+            mode: CupertinoDatePickerMode.date,
+            onDateTimeChanged: _setTextFieldDate,
+          ),
+        ),
+      ),
+    );
+  }
+}
