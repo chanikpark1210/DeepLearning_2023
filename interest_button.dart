2f8916bead680dd715e491b444893d6b6e0c92ad
CHANIK <127841500+chanikpark1210@users.noreply.github.com>
2023-07-01 12:14:43 +0900

    Add files via upload

---
+++ interest_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class InterestButton extends StatefulWidget {
+  const InterestButton({
+    Key? key,
+    required this.interest,
+    required this.onSelectionChanged,
+  }) : super(key: key);
+
+  final String interest;
+  final ValueChanged<bool> onSelectionChanged;
+
+  @override
+  State<InterestButton> createState() => _InterestButtonState();
+}
+
+class _InterestButtonState extends State<InterestButton> {
+  bool _isSelected = false;
+
+  void _onTap() {
+    setState(() {
+      _isSelected = !_isSelected;
+    });
+    widget.onSelectionChanged(_isSelected);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size16,
+          horizontal: Sizes.size24,
+        ),
+        decoration: BoxDecoration(
+          color: _isSelected ? Theme.of(context).primaryColor : Colors.white,
+          borderRadius: BorderRadius.circular(Sizes.size32),
+          border: Border.all(
+            color: Colors.black.withOpacity(0.2),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 5,
+              spreadRadius: 5,
+            ),
+          ],
+        ),
+        child: Text(
+          widget.interest,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: _isSelected ? Colors.white : Colors.black87,
+          ),
+        ),
+      ),
+    );
+  }
+}
